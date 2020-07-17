960deac0b35b5d3b32845f810c5ecc31b4f00292
Fitrah Maulana <fitrahmaulana111@gmail.com>
2020-07-17 16:46:52 +0700

    test1

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,15 +1,36 @@
 function setup() {
   let lat, lon;
 
-  const button = document.getElementById("submit");
-  button.addEventListener("click", async (event) => {
-    const mood = document.getElementById("mood").value;
+  // const button = document.getElementById("submit");
+  // button.addEventListener("click", async (event) => {
+  //   const mood = document.getElementById("mood").value;
+  //   video.loadPixels();
+  //   const image64 = video.canvas.toDataURL();
+  //   const data = {
+  //     lat,
+  //     lon,
+  //     mood,
+  //     image64,
+  //   };
+  //   const options = {
+  //     method: "POST",
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   };
+  //   const response = await fetch("/api", options);
+  //   const json = await response.json();
+  //   console.log(json);
+  // });
+
+  setInterval(async function () {
     video.loadPixels();
     const image64 = video.canvas.toDataURL();
     const data = {
-      lat,
-      lon,
-      mood,
+      // lat,
+      // lon,
+      // mood,
       image64,
     };
     const options = {
@@ -22,7 +43,7 @@ function setup() {
     const response = await fetch("/api", options);
     const json = await response.json();
     console.log(json);
-  });
+  }, 2000);
 
   if ("geolocation" in navigator) {
     console.log("Geolocation is available");
@@ -38,5 +59,5 @@ function setup() {
 
   noCanvas();
   const video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(0, 0);
 }
