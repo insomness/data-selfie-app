018a1c284ff42ee2bb3364793121c0543fc3aa42
Fitrah Maulana <fitrahmaulana111@gmail.com>
2020-07-17 17:11:28 +0700

    fix

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,36 +1,15 @@
 function setup() {
   let lat, lon;
 
-  // const button = document.getElementById("submit");
-  // button.addEventListener("click", async (event) => {
-  //   const mood = document.getElementById("mood").value;
-  //   video.loadPixels();
-  //   const image64 = video.canvas.toDataURL();
-  //   const data = {
-  //     lat,
-  //     lon,
-  //     mood,
-  //     image64,
-  //   };
-  //   const options = {
-  //     method: "POST",
-  //     body: JSON.stringify(data),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-  //   const response = await fetch("/api", options);
-  //   const json = await response.json();
-  //   console.log(json);
-  // });
-
-  setInterval(async function () {
+  const button = document.getElementById("submit");
+  button.addEventListener("click", async (event) => {
+    const mood = document.getElementById("mood").value;
     video.loadPixels();
     const image64 = video.canvas.toDataURL();
     const data = {
-      // lat,
-      // lon,
-      // mood,
+      lat,
+      lon,
+      mood,
       image64,
     };
     const options = {
@@ -43,7 +22,7 @@ function setup() {
     const response = await fetch("/api", options);
     const json = await response.json();
     console.log(json);
-  }, 2000);
+  });
 
   if ("geolocation" in navigator) {
     console.log("Geolocation is available");
